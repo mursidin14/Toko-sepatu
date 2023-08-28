a2ba60c65f51799b55fe98aceacde0d6b737e73b
mursidin14 <54929754+mursidin14@users.noreply.github.com>
2023-08-29 00:59:15 +0800

    about path

--- frontend/src/components/path/About.jsx
+++ frontend/src/components/path/About.jsx
@@ -3,8 +3,23 @@
 
 export default function About() {
     return (
-        <section>
-        
+        <section className="container mt-5 pt-5 mb-5 about">
+            <div className="row">
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                    <div className="card ml-auto">
+                        <img className="card-img-top" src={`./images/about.png`} alt="about"/>
+                    </div>
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                    <div className="card mr-auto pt-5">
+                        <h1 className="font-weigh-bold line-height-1 mb-4">
+                        Toko.RM Shoes <br/>
+                        Since 2023
+                        </h1>
+                        <p className="font-weight-laight text-gray-900 w-75">Toko kami berdiri sejak 2014 hingga sekarang dengan kualitas sepatu terbaik dan harga yang terjangkau, dapatkan penawaran spesial setiap hari sabtu dan minggu.</p>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -1,6 +1,7 @@
 import Header from 'components/path/header';
 import Hero from 'components/path/Hero';
 import Koleksi from 'components/path/Koleksi';
+import About from 'components/path/About';
 
 export default function HomePage() {
     return (
@@ -8,6 +9,7 @@ export default function HomePage() {
             <Header></Header>
             <Hero></Hero>
             <Koleksi />
+            <About />
         </>
     )
 }
